7f418d0408e90bc6cd14a97933d175f30e0fe503
Carlos R. Ueda <carlos.ueda@gmail.com>
2019-09-03 15:06:39 -0400

    Configuring routing for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu/menu.component';
+import { ClientComponent } from './client/client.component';
+import { PetComponent } from './pet/pet.component';
+
+const appRoutes: Routes = [
+  {path: 'pet', component: PetComponent},
+  {path: 'client', component: ClientComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    ClientComponent,
+    PetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pet/pet.component.spec.ts
+++ src/app/pet/pet.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MenuComponent } from './menu.component';
+import { PetComponent } from './pet.component';
 
-describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
+describe('PetComponent', () => {
+  let component: PetComponent;
+  let fixture: ComponentFixture<PetComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      declarations: [ PetComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MenuComponent);
+    fixture = TestBed.createComponent(PetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
